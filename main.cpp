e8410b126848668388648d16009198a2fbbe2783
valentingushchin <valentingushchin@yandex.ru>
2017-06-25 00:44:58 +0300

    add json format

--- main.cpp
+++ main.cpp
@@ -1,42 +1,84 @@
-#include <QTextCodec>
+#include <QCoreApplication>
+#include <QTextStream>
+#include <QCommandLineParser>
 #include <QString>
-#include <iostream>
+#include <QJsonObject>
+#include <QJsonDocument>
 #include "vl/generic.h"
 #include "vl/info1cd.h"
 
-void help()
-{
-
-}
-
 int main(int argc, char *argv[])
 {
-	Q_UNUSED(argc);
-	Q_UNUSED(argv);
+	QCoreApplication app(argc, argv);
+	QCoreApplication::setApplicationName("info1cd");
+	QCoreApplication::setApplicationVersion("1.0");
 
-	QTextCodec *outcodec = QTextCodec::codecForName("CP866");
+	QCommandLineParser parser;
+	parser.setApplicationDescription("\ninfo1cd v1.0 - Trying to find out information about the 1CD file\n"
+					 "Valentin Gushchin (valentingushchin@gmail.com) : 24.06.2017\n"
+					 "https://github.com/valentingushchin/info1cd");
+	parser.addHelpOption();
+	parser.addVersionOption();
 
-	QString fileName = "c:/dev/1CBases/4/1Cv8.1CD";
-//        QString fileName = "d:/1/1Cv8.1CD";
+	QCommandLineOption jsonOption(QStringList() << "j" << "json",
+		    QCoreApplication::translate("main", "Set the output format to json."));
+
+	parser.addOption(jsonOption);
+
+	parser.addPositionalArgument("source", QCoreApplication::translate("main", "Full path to *.1CD file."));
+
+	parser.process(app);
+
+	const QStringList args = parser.positionalArguments();
+
+	bool jsonFormat = parser.isSet(jsonOption);
+
+	if (args.isEmpty()) {
+		parser.showHelp(-100);
+	}
+
+	QString fileName = args.at(0);
+
+	QTextStream cerr(stderr);
 
 	vl::Info1Cd db(fileName);
 	if (!db.readStructureBase()) {
-		std::cerr << "Error!" << std::endl;
+		cerr << "Error parsing" << endl;
 		return -1;
 	}
 
-	std::string isModified = db.getIsModified() ? "yes" : "no";
-	std::string updateIsCompleted = db.getUpdateNotCompleted() ? "yes" : "no";
+	QTextStream cout(stdout);
+	cout.setCodec("CP866");
 
-	std::cout << "ConfigName: " << outcodec->fromUnicode(db.getConfigName()).constData() << std::endl;
-	std::cout << "ConfigVersion: " << outcodec->fromUnicode(db.getConfigVersion()).constData() << std::endl;
-	std::cout << "Modified: " << isModified << std::endl;
-	std::cout << "UpdateNotCompleted: " << updateIsCompleted << std::endl;
-	std::cout << "FormatVersion: " << outcodec->fromUnicode(db.getFormatVersionStr()).constData() << std::endl;
-	std::cout << "BlockSize: " << db.getBlockSize() << std::endl;
-	std::cout << "TableCount: " << db.getTableCount() << std::endl;
-	std::cout << "Locale: " << outcodec->fromUnicode(db.getLocale()).constData() << std::endl;
-	std::cout << "FileSize: " << outcodec->fromUnicode(vl::getFileSizeStr(db.getFileSize(), "Mb", true)).constData() << std::endl;
+	if (jsonFormat) {
+		QJsonObject jso;
+		jso["ConfigName"] = db.getConfigName();
+		jso["ConfigVersion"] = db.getConfigVersion();
+		jso["Modified"] = db.getIsModified();
+		jso["UpdateNotCompleted"] = db.getUpdateNotCompleted();
+		jso["FormatVersion"] = db.getFormatVersionStr();
+		jso["BlockSize"] = static_cast<int>(db.getBlockSize());
+		jso["TableCount"] = static_cast<int>(db.getTableCount());
+		jso["Locale"] = db.getLocale();
+		jso["FileSize"] = db.getFileSize();
 
-	return 0;
+		QJsonDocument jsd(jso);
+		cout << QString::fromUtf8(jsd.toJson()) << endl;
+
+	} else {
+		QString isModified = db.getIsModified() ? "yes" : "no";
+		QString updateIsCompleted = db.getUpdateNotCompleted() ? "yes" : "no";
+
+		cout << "ConfigName: " << db.getConfigName() << endl;
+		cout << "ConfigVersion: " << db.getConfigVersion() << endl;
+		cout << "Modified: " << isModified << endl;
+		cout << "UpdateNotCompleted: " << updateIsCompleted << endl;
+		cout << "FormatVersion: " << db.getFormatVersionStr() << endl;
+		cout << "BlockSize: " << db.getBlockSize() << endl;
+		cout << "TableCount: " << db.getTableCount() << endl;
+		cout << "Locale: " << db.getLocale() << endl;
+		cout << "FileSize: " << vl::getFileSizeStr(db.getFileSize(), "Mb", true) << endl;
+	}
+
+	return app.exec();
 }
